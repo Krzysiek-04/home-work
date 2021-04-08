30e88d6d0483c5a36978533d78d10ee657988b2a
Krzysiek-04 <80480403+Krzysiek-04@users.noreply.github.com>
2021-04-08 23:08:38 +0200

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,145 @@
+// zadanie 1
+const name ='Karol';
+const age = 40;
+const isOpen = 'true';
+const stringBoolen = 'true';
+const stringNumber = 100;
+
+// zadanie 2
+console.log('Michał');
+console.log('Hello Ania');
+console.log('4');
+console.log('false');
+
+// zadanie 3
+console.log('name');
+console.log('age');
+console.log('isOpen');
+console.log('stringBoolen');
+console.log('stringNumber');
+
+// zadanie 4
+console.log(2+2);
+console.log('Michał' + 'Kowalski');
+console.log(2+2*2);
+
+// zadanie 5
+console.log("wynik, " + (8 + 5) + " <---")
+
+// zadanie 6
+let colour = 'blue'; 
+colour = 'black';
+console.log(colour)
+
+// zadanie 7
+function sum() {
+return 2+2;
+}
+function helloWorld() {
+return 'Hello World';
+}
+function boolenTrue() {
+return true;
+}
+function modulo() {
+return 6 % 4;
+}
+function power() {
+return 3 ** 4;
+}
+function hello() {
+return 'Hello,' + 'How are you?';
+}
+function multiplic() {
+return 5 * 60;
+}
+function multiplic2() {
+return 3 * 3600;
+}
+function multiplic3() {
+return 10 * 9 / 2;
+}
+function multiplic4() {
+return 200 - 10;
+}
+
+// zadanie 8
+const nr = [1, 2, 3]
+const cars = ['BMW', 'Audi', 'Rover', 'VW', 'Ford']
+const app = [true, false, true]
+const empty =[];
+const mix = [1, 'me', true, false, 'next']
+console.log(mix[1])
+
+let animals = ['tiger','maus', 'dog']
+console.log(animals.lenght);
+let nothing = [];
+empty.push('Rocky');
+console.log('Rocky');
+
+// zadanie 9
+let obj = {};
+let person = {
+name: 'Iza',
+};
+let personDetalis = {
+name: 'Ola',
+lastName: 'Pinkiewicz',
+age: '27',
+drivingLicence: true,
+}
+console.log(personDetalis['age']);
+console.log(personDetalis);
+
+// zadanie  10
+function add(a, b) {
+return a + b;
+}
+function string(napis) {
+return napis;
+}
+function or(sample) {
+return true;
+}
+function modulo(a, b) {
+return a % b;
+}
+function power(a, b) {
+return a ** b;
+}
+function strings(first, second) {
+return first + second;
+}
+function time(minute) {
+return minute * 60;
+}
+function time2(hour) {
+return hour * 3600
+}
+function tringle(a, b){
+return a * b * 0.5;
+}
+function days(years) {
+return days * 365;
+}
+function power2 (a) {
+return a ** a;
+}
+function tringle2(side1, side2) {
+return side1 + side2 - 1;
+}
+function hello(name) {
+return 'Cześć' + name;
+}
+function age2(date) {
+return 1994 - date;
+}
+function points(a, b) {
+return a * 2 + b * 3
+}
+
+
+
+
+
+
